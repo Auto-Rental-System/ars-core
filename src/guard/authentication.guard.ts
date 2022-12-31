7c9101b581b4a7d741e54774b87f4c9b12c441c3
Uryn Dmytro <dmytro@digia.co>
2022-12-31 19:42:51 +0200

    small changes

--- src/guard/authentication.guard.ts
+++ src/guard/authentication.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable, ExecutionContext, CanActivate, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 
 import { UserService } from 'service/user';
-import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
